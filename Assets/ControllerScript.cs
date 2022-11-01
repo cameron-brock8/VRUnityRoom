95a74152a7f8bcd6aad350c1b80e3255416cbca5
Cameron Brock <ckb45@yahoo.com>
2022-11-01 13:14:12 -0400

    Working on pop-up window

---
+++ Assets/ControllerScript.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerScript : MonoBehaviour
+{
+
+    void Show(Animator animator)
+    {
+        animator.SetBool("Visible", true);
+    }
+
+    void Hide(Animator animator)
+    {
+        animator.SetBool("Visible", false);
+    }
+}
